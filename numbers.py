f8967bc1f0054ca9f743d5a84653a500b5a2896e
Oscar Chu <oscar.7@hotmail.co.uk>
2025-10-12 00:13:54 +0100

    changes to numbers.py

--- numbers.py
+++ numbers.py
@@ -1,31 +1,23 @@
 # floats, ints, decimals, complex numbers (j)
-# addition +
-# Subtraction -
-# Multiplication *
-# Division (float) /
-# Divions (int) //
-# Modulus %
-# Exponent **
+# addition +, subtraction -, multiplication *, division (float) /, (int) //
+# modulus %, exponent **
+# augmented assignment operator: (x = x + 3) same as (x += 3)
+# functions e.g. round(), abs()
 
-
-# Augmented assignment operator (x = x + 3) same as (x += 3)
-
-
-# functions e.g. round, abs,
 round_number = round(2.9)
 # print(round_number)
 
-# math module has many functions @import math
+# math module has many functions — import using: import math
 
 # The input function will prompt the user to enter an input
 x = input("x: ")
-# The above input is currently as string. String cannot be added to int. Therefore below must be cast to int
+
+# The above input is a string. To use it as a number, cast to int
 y = int(x) + 1
-z = (f"x: {x}. y: {y}")
+z = f"x: {x}, y: {y}"
 
-
-# booleans Falsy =  ("", 0, None). ALL strings are true
+# Booleans: Falsy values = ("", 0, None). All non-empty strings are True
 b = bool("")
 
-
 print(z)
+print(f"Boolean value of empty string: {b}")
