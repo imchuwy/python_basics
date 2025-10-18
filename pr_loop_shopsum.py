aa7b4b3e539674914b27728db5239125564cb7f9
Oscar Chu <oscar.7@hotmail.co.uk>
2025-10-18 22:46:00 +0100

    loop projects1

---
+++ pr_loop_shopsum.py
@@ -0,0 +1,5 @@
+price = [23, 30, 56, 25]
+cost = 0
+for item in price:
+    cost += item
+print(f"Total: GBP{cost}")
---
+++ pr_loopshape.py
@@ -0,0 +1,10 @@
+#1h55m https://www.youtube.com/watch?v=_uQrJ0TkZlc&list=PLTjRvDozrdlxj5wgH4qkvwSOdHLOCx10f
+#make and F shape with printing X. Dont use x * number
+numbers = [5, 2, 5, 2, 2]
+
+for i in numbers:
+    output = ''
+    x = range(i)
+    for n in x:
+        output = output + 'x'
+    print(output)
